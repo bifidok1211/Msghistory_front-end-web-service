4bfaed4c052118269897a8ba0e1fcb944ecce5d2
bifidok1211 <iliaru121105@gamil.com>
2025-12-01 11:31:06 +0300

    h-page-fix

--- msg_history-frontend/src/main.tsx
+++ msg_history-frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
