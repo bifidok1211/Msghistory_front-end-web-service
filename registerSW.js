e18e22adbd19591e723c9ffdcab717e79c326890
bifidok1211 <iliaru121105@gamil.com>
2025-12-08 00:56:29 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/RIP_front-end-web-service/sw.js', { scope: '/RIP_front-end-web-service/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Msghistory_front-end-web-service/sw.js', { scope: '/Msghistory_front-end-web-service/' })})}
